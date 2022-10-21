9733e4f4a51876d8c7ca79e04f15798ba2054263
salim <salimsaudagar84@gmail.com>
2022-10-21 21:46:57 +0530

    update changes

--- exp1_1.java
+++ exp1_1.java
@@ -1,7 +1,6 @@
 public class exp1_1 {
     public static void main(String []args) {
-        
-    
+
         System.out.println("\nProgram is showing even no.");
         for(int i=2; i<20; i++){
             if(i%2==0){
