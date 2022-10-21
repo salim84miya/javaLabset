725136561e8d7b3c142dc8367b51dd42f6d89412
salim <salimsaudagar84@gmail.com>
2022-10-21 21:50:42 +0530

    pattern generation

---
+++ PatternGeneration.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class PatternGeneration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+
+}
